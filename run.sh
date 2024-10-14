48678f7a9d983e2faf745f03f2638839fb4e225a
Javier Peral <javiperalb@gmail.com>
2024-10-14 12:33:50 -0300

    GDB Finish

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
+# #!/bin/bash
+# qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 #!/bin/bash
-# Set audio device variable !!
-
-if [ "$(uname)" == "Darwin" ]; then
-    audio="coreaudio"
-else
-    audio="pa"
-fi
-
+# Set audio device variable
 if [[ "$1" = "gdb" ]]; then
-    sudo qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 --rtc base=localtime -d int
 else
-    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 --rtc base=localtime
 fi
